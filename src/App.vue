08a4d885783478e27ac110f29e7556530e07103c
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-10 17:21:33 -0500

    Navbar added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@
 // 4. Include an edit button and form on the dedicated post page that allows admins to edit it (not actual possible to edit post resource on https://jsonplaceholder.typicode.com/) but you can fake it however you'd like
 // 5. Use the Options API at least once (but also feel free to use CAPI if desired)
 // 6. Feel free to work directly in this StackBlitz project or start your own project locally and share it via Github
+import TheNavbar from './components/TheNavbar.vue';
+export default {
+  components: {
+    TheNavbar
+  }
+}
 </script>
 <template>
-  <RouterView />
+  <header>
+    <TheNavbar />
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="navbar-brand">The Vue Post</div>
+      <router-link to="/" class="nav-link navbar-text me-auto">Home</router-link>
+    </div>
+  </nav>
+</template>
